a5e332de7593f2d266f3ce72d55c433316102439
asad8855 <aasady88@gmail.com>
2017-08-06 23:13:46 -0400

    deleting db

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['robovest'],
     include_package_data=True,
     install_requires=[
-        'flask','nltk',
+        'flask'
     ],
     setup_requires=[
         'pytest-runner',
